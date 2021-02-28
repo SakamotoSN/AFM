6f1d5e51562a8ed144820dd9e3733286e5b161a3
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2021-02-28 19:55:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ if (message.author.bot) return; //le bot ne peu pas prendre ces message
   }else{
     
   if ( (texte.toLowerCase()) === ('quoi') || (texte.toLowerCase()) === ('quoi?') || (texte.toLowerCase()) === ('quoi!') || (texte.toLowerCase()) === ('quoi.') ){
-    if (message.length > 2000){
+    if (message.length >= 2000){
       //console.log("sa marche ici")
       message.channel.send(`Pour éviter de flood le salon nous avons decider de ne pas citer le message, merci de votre compréhension"\n${message.author}`, {
         files: [
@@ -62,7 +62,7 @@ if (message.author.bot) return; //le bot ne peu pas prendre ces message
 
 
     if ( (texte.toLowerCase()) === ('non') || (texte.toLowerCase()) === ('non?') || (texte.toLowerCase()) === ('non!') || (texte.toLowerCase()) === ('non.') ){
-      if (message.length > 2000){
+      if (message.length >= 2000){
         message.channel.send(`Pour éviter de flood le salon nous avons decider de ne pas citer le message, merci de votre compréhension"\n${message.author}`, {
             files: [
               "./folder/bril.mp4"
@@ -83,7 +83,7 @@ if (message.author.bot) return; //le bot ne peu pas prendre ces message
 
     
       if ( (texte.toLowerCase()) === ('oui') || (texte.toLowerCase()) === ('oui?') || (texte.toLowerCase()) === ('oui!') || (texte.toLowerCase()) === ('oui.') ){
-      if (message.length > 2000){
+      if (message.length >= 2000){
         message.channel.send(`Pour éviter de flood le salon nous avons decider de ne pas citer le message, merci de votre compréhension"\n${message.author}`, {
             files: [
               "./folder/stiti.mp4"
