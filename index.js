a859edc0ea18ae0e94061e0992d2c81725d930ac
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2021-02-28 20:46:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ if (message.author.bot) return; //le bot ne peu pas prendre ces message
       return;
   }else{
     
-  if ( (texte.toLowerCase()) === ('quoi') || (texte.toLowerCase()) === ('quoi?') || (texte.toLowerCase()) === ('quoi!') || (texte.toLowerCase()) === ('quoi.') || ('pourquoi') || (texte.toLowerCase()) === ('pourquoi?') || (texte.toLowerCase()) === ('pourquoi!') || (texte.toLowerCase()) === ('pourquoi.') ){
+  if ( (texte.toLowerCase()).includes("quoi")){ //=== ('quoi') || (texte.toLowerCase()) === ('quoi?') || (texte.toLowerCase()) === ('quoi!') || (texte.toLowerCase()) === ('quoi.') || ('pourquoi') || (texte.toLowerCase()) === ('pourquoi?') || (texte.toLowerCase()) === ('pourquoi!') || (texte.toLowerCase()) === ('pourquoi.') ){
     if ((message.content.length) > 1000){
       message.channel.send(`Pour éviter de flood le salon nous avons decider de ne pas citer le message, merci de votre compréhension"\n${message.author}`, {
         files: [
