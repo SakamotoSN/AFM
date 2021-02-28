1e21683051a595a761a2dd21b29bdf3934ea6aa4
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2021-02-28 17:55:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,16 +36,25 @@ if (message.author.bot) return; //le bot ne peu pas prendre ces message
     
   if ( (texte.toLowerCase()) === ('quoi') || (texte.toLowerCase()) === ('quoi?') || (texte.toLowerCase()) === ('quoi!') || (texte.toLowerCase()) === ('quoi.') ){
       console.log("sa marche ici")
-    message.channel.send(`> "${message}" ${message.author} \nhttps://cdn.discordapp.com/attachments/815579872390414339/815581619138068511/feur4k-1.mp4`);
-    } //si quoi a etais detecter (meme avec des point) alors il envois feur
+      message.channel.send(`> "${message}" ${message.author} `, {
+        files: [
+          "./folder/feur.mp4"
+        ]
+      })}
   
     if ( (texte.toLowerCase()) === ('non') || (texte.toLowerCase()) === ('non?') || (texte.toLowerCase()) === ('non!') || (texte.toLowerCase()) === ('non.') ){
-    message.channel.send(`> "${message}" ${message.author} \nhttps://cdn.discordapp.com/attachments/815579872390414339/815581691195162627/video_1.mp4`);
-    }
+        message.channel.send(`> "${message}" ${message.author} `, {
+            files: [
+              "./folder/bril.mp4"
+            ]
+          })}
   
     if ( (texte.toLowerCase()) === ('oui') || (texte.toLowerCase()) === ('oui?') || (texte.toLowerCase()) === ('oui!') || (texte.toLowerCase()) === ('oui.') ){
-    message.channel.send(`> "${message}" ${message.author} \n https://cdn.discordapp.com/attachments/815579872390414339/815581559376576522/stiti.mp4`);
-    }
+        message.channel.send(`> "${message}" ${message.author} `, {
+            files: [
+              "./stiti/feur.mp4"
+            ]
+          })}
   }
   
  }
