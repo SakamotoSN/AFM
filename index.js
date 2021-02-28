57cb4c6772120f70fcbcef3196143a450eb59d1a
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2021-02-28 20:18:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -35,8 +35,7 @@ if (message.author.bot) return; //le bot ne peu pas prendre ces message
   }else{
     
   if ( (texte.toLowerCase()) === ('quoi') || (texte.toLowerCase()) === ('quoi?') || (texte.toLowerCase()) === ('quoi!') || (texte.toLowerCase()) === ('quoi.') ){
-    console.log((message.length + message.author.length))
-    if ((message.length) > 1000){
+    if ((message.length) < 1000){
       //console.log("sa marche ici")
       message.channel.send(`Pour éviter de flood le salon nous avons decider de ne pas citer le message, merci de votre compréhension"\n${message.author}`, {
         files: [
