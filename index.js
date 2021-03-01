d4f985227ff94e0f28f894309682ffea1193edab
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2021-03-01 17:33:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ bot.on('message', message => {
   console.log(`entrer ${test}`)
   var texte = test[test.length - 1];
   console.log(`dernier mot : ${texte}`) //prend que le dernier mots
-  if((texte.content('?')) || (texte.content('!')) ||(texte.content('.')) ){
+  if((texte.toLowerCase().includes('?'), (texte.lenght-1) ) || (texte.toLowerCase().includes('!'), (texte.lenght-1) ) ||(texte.toLowerCase().includes('.'), (texte.lenght-1)) ){
     var texte = test[test.length - 2];
     console.log("un ? a etais detecter dans le code" )
     console.log(texte)
