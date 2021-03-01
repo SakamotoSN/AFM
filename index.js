5274b2b8ca09e99644639bbf53f1540b9d206681
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2021-03-01 19:04:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ bot.on('message', message => {
 
 
         console.log("passage !")
-    if ( (texte.toLowerCase()) === ("quoi", (texte.lenght-4) ) ||  (texte.toLowerCase()).includes("quoi.", (texte.lenght-5) ) ||  (texte.toLowerCase()).includes("quoi?", (texte.lenght-5) )||  (texte.toLowerCase()).includes("quoi!", (texte.lenght-5) )){
+    if ( (texte.toLowerCase()) === ("quoi", (texte.lenght-5) ) ||  (texte.toLowerCase()).includes("quoi.", (texte.lenght-6) ) ||  (texte.toLowerCase()).includes("quoi?", (texte.lenght-6) )||  (texte.toLowerCase()).includes("quoi!", (texte.lenght-6) )){
     if ((message.content.length) > 200){
       message.channel.send('```Pour éviter de flood le salon nous avons decider de ne pas citer le message, merci de votre compréhension"\n${message.author}```', {
         files: [
