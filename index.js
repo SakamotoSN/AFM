b5e5a6d4c256c0ec19648ad77b37948ac6be581b
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2021-03-01 13:48:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ if (message.author.bot) return; //le bot ne peu pas prendre ces message
   }else{
     
     if ( (texte.toLowerCase()).includes("quoi", (texte.lenght-5) )){
-    if ((message.content.length) > 1000){
+    if ((message.content.length) > 300){
       message.channel.send(`Pour éviter de flood le salon nous avons decider de ne pas citer le message, merci de votre compréhension"\n${message.author}`, {
         files: [
           "./folder/feur.mp4"
@@ -61,7 +61,7 @@ if (message.author.bot) return; //le bot ne peu pas prendre ces message
 
 
     if ( (texte.toLowerCase()).includes("non", (texte.lenght-4) )){
-      if (message.content.length > 1000){
+      if (message.content.length > 300){
         message.channel.send(`Pour éviter de flood le salon nous avons decider de ne pas citer le message, merci de votre compréhension"\n${message.author}`, {
             files: [
               "./folder/bril.mp4"
@@ -82,7 +82,7 @@ if (message.author.bot) return; //le bot ne peu pas prendre ces message
 
     
       if ( (texte.toLowerCase()).includes("oui", (texte.lenght-4) )){
-      if (message.content.length > 1000){
+      if (message.content.length > 300){
         message.channel.send(`Pour éviter de flood le salon nous avons decider de ne pas citer le message, merci de votre compréhension"\n${message.author}`, {
             files: [
               "./folder/stiti.mp4"
