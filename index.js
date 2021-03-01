c3d534c3814ac237a89c6298ef4f38f53278bf43
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2021-03-01 12:23:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ if (message.author.bot) return; //le bot ne peu pas prendre ces message
       return;
   }else{
     
-    if ( (texte.toLowerCase()).includes("quoi", (texte.lenght-5) )){
+    if ( (texte.toLowerCase()).includes("quoi", (texte.lenght-6) )){
     if ((message.content.length) > 1000){
       message.channel.send(`Pour éviter de flood le salon nous avons decider de ne pas citer le message, merci de votre compréhension"\n${message.author}`, {
         files: [
@@ -60,7 +60,7 @@ if (message.author.bot) return; //le bot ne peu pas prendre ces message
 
 
 
-    if ( (texte.toLowerCase()).includes("non", (texte.lenght-4) )){
+    if ( (texte.toLowerCase()).includes("non", (texte.lenght-5) )){
       if (message.content.length > 1000){
         message.channel.send(`Pour éviter de flood le salon nous avons decider de ne pas citer le message, merci de votre compréhension"\n${message.author}`, {
             files: [
@@ -81,7 +81,7 @@ if (message.author.bot) return; //le bot ne peu pas prendre ces message
 
 
     
-      if ( (texte.toLowerCase()).includes("oui", (texte.lenght-4) )){
+      if ( (texte.toLowerCase()).includes("oui", (texte.lenght-5) )){
       if (message.content.length > 1000){
         message.channel.send(`Pour éviter de flood le salon nous avons decider de ne pas citer le message, merci de votre compréhension"\n${message.author}`, {
             files: [
