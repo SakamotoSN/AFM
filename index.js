3aadf7aa75b0cfe9d7f02c7c6e41a2d95d71983d
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2021-05-04 14:18:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,13 +18,6 @@ bot.on('message', message => {
 
 
 
-  var CronJob = require('cron').CronJob;
-  var job = new CronJob('* * * * * *', function() {
-    console.log('You will see this message every second');
-  }, null, true, 'America/Rainy_River');
-  job.start();
-  
-
 
 
 
