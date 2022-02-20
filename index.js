441167a5fe32349d7721b7cc2e5e50df63b996c8
SakamotoSN <38160383+SakamotoSN@users.noreply.github.com>
2022-02-20 17:53:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -65,51 +65,26 @@ bot.on('message', message => {
 
 
   if(message.content === "!tf"){
-    message.channel.send({ embeds: [{
-      color: 3447003,
-      author: {
-        name: "Author Name, it can hold 256 characters",
-        icon_url: "https://i.imgur.com/lm8s41J.png"
-      },
-      thumbnail: {
-        url: "http://i.imgur.com/p2qNFag.png"
-      },
-      image: {
-        url: "http://i.imgur.com/yVpymuV.png"
-      },
-      title: "This is your title, it can hold 256 characters",
-      url: "https://discord.js.org/#/docs/main/master/class/MessageEmbed",
-      description: "This is the main body of text, it can hold 2048 characters.",
-      fields: [{
-        name: "This is a single field title, it can hold 256 characters",
-        value: "This is a field value, it can hold 1024 characters.",
-        inline: false
-      },
-      {
-        name: "Inline fields",
-        value: "They can have different fields with small headlines, and you can inline them.",
-        inline: true
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](https://discord.js.org/#/docs/main/master/class/MessageEmbed) inside of rich embeds.",
-        inline: true
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them.",
-        inline: true
-      },
-      {
-        name: "\u200b",
-        value:"\u200b"
-      }],
-      timestamp: new Date(),
-      footer: {
-        icon_url: "http://i.imgur.com/w1vhFSR.png",
-        text: "This is the footer text, it can hold 2048 characters"
-      }
-    }]});
+
+    const embed = new MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setTimestamp()
+	.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+  channel.send(embed)
 }
 
 
