80b5ab99d7c145d6b6b4f8046b373491c7b06748
SakamotoSN <38160383+SakamotoSN@users.noreply.github.com>
2022-02-20 18:28:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ bot.on('message', message => {
 
   if(message.content === "!date"){
   let date = JSON.stringify(text).split(`","`);
-  message.channel.send(JSON.stringify(date[0]).slice('15','-1'))
+  message.channel.send(JSON.stringify(date[0]).slice('16','-1'))
   message.channel.send(JSON.stringify(date[1])) 
   message.channel.send(JSON.stringify(date[2]))
   message.channel.send(JSON.stringify(date[3]))
@@ -69,7 +69,7 @@ bot.on('message', message => {
     const embed = new MessageEmbed()
 	.setColor('#0099ff')
 	.setTitle('Some title')
-	.setAuthor('De ' + JSON.stringify(date[1]), 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+	.setAuthor('De ' + JSON.stringify(date[1]), client.get_user(JSON.stringify(date[0]).slice('16','-1')).avatar_url)
 	.setDescription('Some description here')
 	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
 	.setImage('https://i.imgur.com/AfFp7pu.png')
