b3e50f84aab6a35fda9ce39bf9d102707d59d638
SakamotoSN <38160383+SakamotoSN@users.noreply.github.com>
2022-02-20 19:15:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -85,11 +85,11 @@ bot.on('message', message => {
 
 if(message.content === "!debug"){
   let split = JSON.stringify(text).split(`","`);
-  var ID = JSON.stringify(split[0]).slice('16','-1') 
-  var auteur = JSON.stringify(split[1]).slice('10','-1')
-  var date = JSON.stringify(split[2]).slice('9','-1')
-  var mess = JSON.stringify(split[3]).slice('12','-1')
-  var image = JSON.stringify(split[4]).slice('16','-1')
+  var ID = JSON.stringify(split[0]).slice('17','-1') 
+  var auteur = JSON.stringify(split[1]).slice('11','-1')
+  var date = JSON.stringify(split[2]).slice('10','-1')
+  var mess = JSON.stringify(split[3]).slice('13','-1')
+  var image = JSON.stringify(split[4]).slice('17','-1')
 
 
   message.channel.send(ID)
