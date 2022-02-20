1e4a3331a2e781be2c498084bf4ea3428cef55fa
SakamotoSN <38160383+SakamotoSN@users.noreply.github.com>
2022-02-21 00:15:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ console.log(rdm)
     const embed = new MessageEmbed()
 	.setColor('#0099ff')
 	.setAuthor('De ' + auteur)
-	.setDescription((((mess.replace(/(?:\\[rn]|[\r\n]+)+/g, "\n")).replace('\\', "")).replace("\\", " ").replaceAll("\\", " ")))
+	.setDescription((((mess.replace(/(?:\\[rn]|[\r\n]+)+/g, "\n")).replace("\\", " ").replaceAll("\\", " ")))
 	.setImage(image)
 	.setFooter(date + " || " + ID);
 
