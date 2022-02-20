8e7127b6a702d0ea9ec0b388aec36530c664c667
SakamotoSN <38160383+SakamotoSN@users.noreply.github.com>
2022-02-20 19:57:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ if(message.content === "!debug"){
   message.channel.send(ID)
   message.channel.send(auteur)
   message.channel.send(date)
-  message.channel.send(mess.replace(/(?:\\[rn])+/g, ""));
+  message.channel.send(mess.replace(/(?:\\[rn])+/g, "\n"));
   message.channel.send(image)
 }
 
