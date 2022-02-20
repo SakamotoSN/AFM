2f2c4f3ca1ef704375490d03a02d57064b191bcf
SakamotoSN <38160383+SakamotoSN@users.noreply.github.com>
2022-02-20 15:57:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -40,29 +40,13 @@ bot.login(process.env.DISCORD_TOKEN);
 bot.on('message', message => {
 
   if(message.content === "!rdm"){
-  text = JSON.stringify(csv[0])
+  text = csv[0].slice();
 
 
 
-  fs.createReadStream(text)
-  .pipe(
-    parse({
-      delimiter: ','
-    })
-  )
-  .on('data', function (dataRow2) {
-    csvData.push(dataRow2);
-  })
-  .on('end', function () {
-    console.log("teste2");
-    csv2 = csvData2;
-    console.log(csv2);
-
-  });
 
 
-
-   message.channel.send(JSON.stringify(csv2[1]))  
+   message.channel.send(JSON.stringify(text[1]))  
 
    //let args = text.split(",").slice(1)
 
