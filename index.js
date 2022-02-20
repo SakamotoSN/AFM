b4ebdb905ae0152d111b851372b3a23c24d200d3
SakamotoSN <38160383+SakamotoSN@users.noreply.github.com>
2022-02-20 20:59:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const parse = require('csv-parser');
 const fs = require('fs');
 const csvData = [];
 var csv = [];
-var alea = 0; 
 var statue = "idle" //online
 bot.on('ready', function() {
   bot.user.setPresence({ game: { name: 'Test'}, status: `${statue}`})
@@ -35,28 +34,43 @@ fs.createReadStream('./folder/Dekuali.csv')
 bot.login(process.env.DISCORD_TOKEN);
 
 bot.on('message', message => {
-  
+  var text = csv[3]//mess brute
+
+
+
+  if(message.content === "!rdm"){
+   message.channel.send(JSON.stringify(text))  
+  }
+
+
+
+
+  if(message.content === "!date"){
+  let date = JSON.stringify(text).split(`","`);
+  message.channel.send(JSON.stringify(date[1])) 
+  message.channel.send(JSON.stringify(date[2]))
+  message.channel.send(JSON.stringify(date[3]))
+  message.channel.send(JSON.stringify(date[4]))
+  }
+
+
+
+
+
+
+
+
+
+ 
+
+
 
   if(message.content === "!tf"){
-    function random(min, max) {
-      min = Math.ceil(0)// 0 est impossible a avoir
-      max = Math.floor(10)
-      alea = Math.floor(Math.random() * (max - min +1) + min);
-   }
-   random();
-
-   console.log(alea);
-//debug
-   message.channel.send(ID)
-   message.channel.send(auteur)
-   message.channel.send(date)
-   message.channel.send((mess)    )//.replace(/(?:\\[rn]|[\r\n]+)+/g, "")).replace('\\', '\n\n'))
-   //message.channel.send(image)
-//debug
 
 
-var text = csv[5]//mess brute
 
+
+    
     let split = JSON.stringify(text).split(`","`);
     var ID = JSON.stringify(split[0]).slice('17','-1') 
     var auteur = JSON.stringify(split[1]).slice('12','-1')
@@ -67,35 +81,28 @@ var text = csv[5]//mess brute
     const embed = new MessageEmbed()
 	.setColor('#0099ff')
 	.setAuthor('De ' + auteur)
-	.setDescription(mess.replace(/(?:\\[rn]|[\r\n]+)+/g, "").replace('\\', '\n\n'))
+	.setDescription(mess)
 	.setImage(image)
 	.setFooter(date + " || " + ID);
 
   message.channel.send(embed)
 }
 
+if(message.content === "!debug"){
+  let split = JSON.stringify(text).split(`","`);
+  var ID = JSON.stringify(split[0]).slice('17','-1') 
+  var auteur = JSON.stringify(split[1]).slice('12','-1')
+  var date = JSON.stringify(split[2]).slice('10','-1')
+  var mess = JSON.stringify(split[3]).slice('13','-1')
+  var image = JSON.stringify(split[4]).slice('17','-1')
 
 
-
-
-
-
-
-// if(message.content === "!debug"){
-//   let split = JSON.stringify(text).split(`","`);
-//   var ID = JSON.stringify(split[0]).slice('17','-1') 
-//   var auteur = JSON.stringify(split[1]).slice('12','-1')
-//   var date = JSON.stringify(split[2]).slice('10','-1')
-//   var mess = JSON.stringify(split[3]).slice('13','-1')
-//   var image = JSON.stringify(split[4]).slice('17','-1')
-
-
-//   message.channel.send(ID)
-//   message.channel.send(auteur)
-//   message.channel.send(date)
-//   message.channel.send((mess.replace(/(?:\\[rn]|[\r\n]+)+/g, "")).replace('\\', '\n\n'))
-//   //message.channel.send(image)
-// }
+  message.channel.send(ID)
+  message.channel.send(auteur)
+  message.channel.send(date)
+  message.channel.send((mess.replace(/(?:\\[rn]|[\r\n]+)+/g, "")).replace('\\', '\n\n'))
+  //message.channel.send(image)
+}
 
 
 
